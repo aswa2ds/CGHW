f62e2f1bf2b61db9f2af7077d5789fa663efe319
edwardmartin927 <edwardmartin927@gmail.com>
2018-10-26 10:10:49 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(1.0 * 0.5762 / 6.41);
+        EventQueue.invokeLater(()->{
+            Frame frame = new Frame();
+        });
+    }
+}
